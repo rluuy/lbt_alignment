fb050614b5e2e422865caecf60ee25f889fc313e
rluuy <rluu727@gmail.com>
2023-03-22 16:03:45 -0700

    Added Validation Loss

--- convolutional_nn_model.py
+++ convolutional_nn_model.py
@@ -17,10 +17,15 @@ import torchvision.transforms as tf
 from torch.utils.tensorboard import SummaryWriter
 from sklearn.model_selection import train_test_split
 from utils import *
+import matplotlib.pyplot as plt
 
 Learning_Rate=1e-5 # Learning_Rate: is the step size of the gradient descent during the training.
 width=height=100   # Width and height are the dimensions of the image used for training. All images during the training processes will be resized to this size.
 batchSize=1        # batchSize: is the number of images that will be used for each iteration of the training.
+
+'''
+10_Data has 675 Data Samples
+'''
 
 class CNN(nn.Module):
     def __init__(self):
@@ -41,7 +46,7 @@ class CNN(nn.Module):
         return x
 
 # define a custom dataset to load the data
-class CustomDataset(Dataset):
+class LBT_Custom_Dataset(Dataset):
     def __init__(self, dataframe):
         self.data = dataframe[['p_x', 'p_y', 'data_img']]
         self.labels = dataframe[['d_x', 'd_y', 'd_z', 't_x', 't_y']]
@@ -58,24 +63,38 @@ class CustomDataset(Dataset):
         sample = {'img': img, 'px': px, 'py': py, 'label': label}
         return sample
 
+def plot_losses(train_losses, validation_losses):
+    plt.plot(train_losses, label='Training Loss')
+    plt.plot(validation_losses, label='Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
 
 
 
 if __name__ == '__main__':
+    train_losses = []
+    val_losses = []
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print("Can we use CUDA: " + str(torch.cuda.is_available()))
     df = load_dataframe("10_data.pt")
-    train, test = train_test_split(df, test_size=0.2, random_state=42)
 
-    train_dataset = CustomDataset(train)
+
+    train_val, test = train_test_split(df, test_size=0.2, random_state=42)
+    train, val = train_test_split(train_val, test_size=0.25, random_state=42)
+
+    train_dataset = LBT_Custom_Dataset(train)
+    val_dataset = LBT_Custom_Dataset(val)
     dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False)  # Create a validation DataLoader
 
     model = CNN()
     model = model.double()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
     num_epochs = 200
     running_loss = 0.0
-    writer = SummaryWriter('runs/lbt_test1')
+
     for epoch in range(num_epochs):
         for i , batch in enumerate(dataloader,0):
             imgs = batch['img']
@@ -85,21 +104,40 @@ if __name__ == '__main__':
             outputs = model(inputs, pxs, pys)
             labels = batch['label']
             loss = nn.functional.mse_loss(outputs, labels)
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
+        train_losses.append(loss.item())
 
-        print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item()}')
 
+        val_loss = 0.0
+        val_batch_count = 0
+        with torch.no_grad():
+            for i, val_batch in enumerate(val_dataloader, 0):
+                val_imgs = val_batch['img']
+                val_pxs = val_batch['px'].reshape(-1, 1)
+                val_pys = val_batch['py'].reshape(-1, 1)
+                val_inputs = val_imgs
+                val_outputs = model(val_inputs, val_pxs, val_pys)
+                val_labels = val_batch['label']
+                val_batch_loss = nn.functional.mse_loss(val_outputs, val_labels)
+                val_loss += val_batch_loss.item()
+                val_batch_count += 1
+
+        avg_val_loss = val_loss / val_batch_count
+        val_losses.append(avg_val_loss)
+        print(f'Epoch {epoch + 1}/{num_epochs}, Training Loss: {loss.item()}, Validation Loss: {avg_val_loss}')
+
+
+    plot_losses(train_losses, val_losses)
     # Set model to evaluation mode
     model.eval()
 
-    # Initialize test data loader
-    test_dataset = CustomDataset(test)
-    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True)
 
-    # Initialize criterion
+    test_dataset = LBT_Custom_Dataset(test)
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True)
     criterion = nn.MSELoss()
 
     # Initialize total loss and number of samples
@@ -124,8 +162,7 @@ if __name__ == '__main__':
             # Update total loss and number of samples
             total_loss += loss.item() * len(batch)
             n_samples += len(batch)
+            mean_loss = total_loss / n_samples
 
-    # Calculate mean loss
-    mean_loss = total_loss / n_samples
 
-    print(f"Test mean squared error: {mean_loss:.4f}")
+        print(f"Test mean squared error: {mean_loss:.4f}")
